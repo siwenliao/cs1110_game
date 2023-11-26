d71924182621e23f0b8546332306522631569204
siwenliao <147187525+siwenliao@users.noreply.github.com>
2023-11-26 17:47:25 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,37 @@
+# Partner 1: Siwen Liao (zfc9nu)
+# Partner 2: Alexa Sison (dke7md)
+
+# We are simulating the game Fireboy and Watergirl,
+# but a slightly more simple version.
+# There will be two characters, Fireboy and Watergirl,
+# who navigate through a "forest temple" (similar to Mario)
+# from bottom to top. There will be fire and water as obstacles'
+# for both characters. If they make it to the top,
+# they complete the level and move on.
+
+# 3 basic features:
+
+# User Input: Fireboy will use the right, left, and up arrows to move,
+# and Watergirl will use "S", "E", and "F" on the keyboard to move.
+
+# Game Over: Both characters will have 3 lives combined.
+# If they lose all 3 before they complete the levels, they lose.
+
+# Graphics/Images: We can do graphics of fire and water,
+# graphics of the characters, etc.
+
+# 4 additional features:
+
+# Object-oriented code: We will have a class for the characters,
+# which has 2 instances. We will also have a class for the collectibles.
+
+# Collectibles: We will have collectibles (fire coins for Fireboy
+# and water coins for Watergirl)
+
+# Multiple levels: Similar to how the game Fireboy and Watergirl
+# has many levels, we will also create at least 3 levels.
+
+# Restart from game over: When 3 lives (combined) are lost, players
+# will have an option to restart from the beginning.
+
+
